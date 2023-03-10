a4b7aca13a133aff13ffd80fb5c510187e46bf08
sadegh <sadegh15khedry@gmail.com>
2023-03-10 03:37:31 -0800

    added NAVBAR USING JSX

---
+++ src/MyHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+export default class MyHeader extends React.Component{
+    render(){
+      return(
+      <img src="images/header.jpg" alt='header image' id="header"/>
+      )
+    }
+  }
+  
+  
