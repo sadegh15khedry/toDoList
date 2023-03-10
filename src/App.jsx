625fa7407f3f2d88b50e6efdd562658296bc70e2
sadegh <sadegh15khedry@gmail.com>
2023-03-10 01:36:05 -0800

    added headere image to project using jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Car(props) {
-  return <h2>I am a {props.brand} Car!</h2>;
+function shoot(){
+  alert("shoot");
+}
+class MyHeader extends React.Component{
+  render(){
+    return(
+    <img src="images/header.jpg" alt='header image' id="header" onClick={shoot}/>
+    )
+  }
+}
+
+class CartItem extends React.Component{
+  
+  render(){
+    return(
+      <h3>
+        this is a CartItem
+      </h3>
+    )
+  }
+}
+
+class Cart extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      Id : 1,
+      sumtotal: "0",
+      date : "1401/12/16"
+    }
+  }
+
+  render(){
+    return(
+    <h2>
+      this is a cart and total is  { this.state.sumtotal }
+    </h2>
+    );
+  }
 }
 
 
@@ -13,19 +50,8 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          <Car brand ="bmw" />
-        </a>
-      </header>
-    
+        <MyHeader />
+        <Cart />
     </div>
   );
 }
